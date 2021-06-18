5b77679a225aec8ef4ce188d7ea2c58475845efd
Scott Deeg <sdeeg@vmware.com>
2021-06-18 10:02:03 -0700

    force build

--- src/main/kotlin/io/pivotal/sqlrunner/SQLRunnerInitializer.kt
+++ src/main/kotlin/io/pivotal/sqlrunner/SQLRunnerInitializer.kt
@@ -33,6 +33,7 @@ class SQLRunnerInitializer {
                 jdbcTemplate.execute("create table my_table (id INT, my_data varchar(32))")
                 jdbcTemplate.execute("insert into my_table values ( 1, 'hello, world')")
                 jdbcTemplate.execute("insert into my_table values ( 2, 'foo bar baz')")
+                jdbcTemplate.execute("insert into my_table values ( 3, 'No matter where you go, there you are')")
                 jdbcTemplate.execute("insert into my_table values ( 42, 'life, the universe, everything')")
 
                 jdbcTemplate.execute("create table table2 (id INT, your_data varchar(32))")
