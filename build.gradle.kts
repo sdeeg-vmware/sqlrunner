8c832635a811473f4a2fd707c01a3dbf48abd0c8
sdeeg-vmware <sdeeg@vmware.com>
2023-09-29 14:45:59 -0700

    beta cleanup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,19 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.6.2"
-	id("io.spring.dependency-management") version "1.0.11.RELEASE"
-	kotlin("jvm") version "1.6.10"
-	kotlin("plugin.spring") version "1.6.10"
+	id("org.springframework.boot") version "3.1.4"
+	id("io.spring.dependency-management") version "1.1.3"
+	id("org.graalvm.buildtools.native") version "0.9.27"
+	kotlin("jvm") version "1.8.22"
+	kotlin("plugin.spring") version "1.8.22"
 }
 
 group = "com.vmware.sqlrunner"
-version = "0.3-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_11
+version = "2.0.0-beta-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_17
+}
 
 repositories {
 	mavenCentral()
@@ -17,21 +21,25 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
-	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
 	implementation("org.springframework.boot:spring-boot-starter-jdbc")
+	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	runtimeOnly("com.h2database:h2")
-	runtimeOnly("mysql:mysql-connector-java")
-	runtimeOnly("org.postgresql:postgresql")
+	// runtimeOnly("com.ibm.db2:jcc")
+	// runtimeOnly("com.microsoft.sqlserver:mssql-jdbc")
+	// runtimeOnly("com.mysql:mysql-connector-j")
+	// runtimeOnly("com.oracle.database.jdbc:ojdbc8")
+	// runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
+	// runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.withType<KotlinCompile> {
 	kotlinOptions {
 		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "11"
+		jvmTarget = "17"
 	}
 }
 
