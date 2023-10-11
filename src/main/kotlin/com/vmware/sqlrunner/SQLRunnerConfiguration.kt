be348d156a1a49184ab341aee191cd7a665a3b36
sdeeg-vmware <sdeeg@vmware.com>
2023-10-11 16:16:52 -0700

    add JdbcTemplate create method

--- src/main/kotlin/com/vmware/sqlrunner/SQLRunnerConfiguration.kt
+++ src/main/kotlin/com/vmware/sqlrunner/SQLRunnerConfiguration.kt
@@ -1,6 +1,41 @@
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Bean
+import org.springframework.boot.jdbc.DataSourceBuilder
+import org.springframework.jdbc.core.JdbcTemplate
+import org.slf4j.LoggerFactory
+import javax.sql.DataSource
 
 @Configuration
 public class SQLRunnerConfiguration {
+    private val logger = LoggerFactory.getLogger(javaClass)
+
+    @Bean
+    public fun secondH2JDBCTemplate(): JdbcTemplate {
+        
+        logger.info("Creating second H2 JdbcTemplate")
+        return JdbcTemplate( secondH2() )
+    }
+
+    @Bean
+    public fun secondH2(): DataSource {
+        logger.info("Creating second H2 DataSource")
+        val dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName("org.h2.Driver");
+        dataSourceBuilder.url("jdbc:h2:~/test");
+        dataSourceBuilder.username("sa");
+        dataSourceBuilder.password("");
+        return dataSourceBuilder.build();
+    }
+
+    // @Bean
+    // public fun postgresDataSource(): DataSource {
+    //     logger.info("Creating second DataSource")
+    //     val dataSourceBuilder = DataSourceBuilder.create();
+    //     dataSourceBuilder.driverClassName("org.postgresql.Driver");
+    //     dataSourceBuilder.url("jdbc:postgresql://some_host/database");
+    //     dataSourceBuilder.username("SA");
+    //     dataSourceBuilder.password("");
+    //     return dataSourceBuilder.build();
+    // }
 
 }
