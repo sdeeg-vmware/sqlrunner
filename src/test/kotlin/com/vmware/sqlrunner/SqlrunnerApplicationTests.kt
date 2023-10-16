3cf9dc83dd3209c740b7f6869e6ca673873358dc
sdeeg-vmware <sdeeg@vmware.com>
2023-10-16 13:28:55 -0700

    update to Boot 3 test

--- src/test/kotlin/com/vmware/sqlrunner/SqlrunnerApplicationTests.kt
+++ src/test/kotlin/com/vmware/sqlrunner/SqlrunnerApplicationTests.kt
@@ -5,12 +5,23 @@ package com.vmware.sqlrunner
 // import org.springframework.boot.test.context.SpringBootTest
 // import org.springframework.test.context.junit4.SpringRunner
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.boot.test.context.SpringBootTest
+
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+// import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 // @RunWith(SpringRunner::class)
-// @SpringBootTest
-// class SqlrunnerApplicationTests {
+@SpringBootTest()
+class SqlrunnerApplicationTests {
 
-// 	@Test
-// 	fun contextLoads() {
-// 	}
+	@Test
+	fun contextLoads() {
+	}
 
-// }
+}
